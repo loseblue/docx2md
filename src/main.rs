d780b2d34bd4b8240ac1f9001ed178bc69058d16
baojingzhong <baojingzhong@yunzhiruantong.com>
2023-08-07 18:27:26 +0800

    Get docx.document

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 
 
 fn main() {
+    println!("hello world");
+
     use docx_rust::document::Paragraph;
     use docx_rust::DocxFile;
     
     let docx = DocxFile::from_file("11.docx").unwrap();
     let mut docx = docx.parse().unwrap();
+
+    //println!("{:#?}", docx);
+
+    println!("{:#?}", docx.document);
+
+
+    for (key, value) in docx.headers.iter() {
+        println!("key: {}", key);
+      }
+
     
     let para = Paragraph::default().push_text("Lorem Ipsum");
     docx.document.push(para);
